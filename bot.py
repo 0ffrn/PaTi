659dbdb2b61bb8fe98f8c50b41232b302bd945b2
0ffrn <74686262+0ffrn@users.noreply.github.com>
2020-11-18 23:23:12 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from time import sleep
+import request
+
+print("edita el script y pon el path de firefox donde pone executable_path porfa")
+os.system('anonsurf start, cuando tengas todo listo lo ejecutas')
+
+made_by_elmo = 1
+
+while made_by_elmo == 1
+   r.status_code
+   if r.status_code == 200
+      driver = webdriver.Firefox(executable_path="/usr/share/applications/firefox.desktop")
+      driver.get(https://www.redbull.com/es-es/mejor-segundo-2020)
+      driver.find_element_by_xpath('//button[text()="Blon"]')\
+         .click()
+      os.system('anonsurf changeid')
+      driver.refresh()
+   elif r.status_code == 522
+      os.system('anonsurf changeid')
